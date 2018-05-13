60ebf290742608d77ca300c104d75dcb1a9a2c63
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-13 18:49:26 +0530

    --initial commit--

---
+++ CntrlFlowNdLooping/ControlDemo.py
@@ -0,0 +1,19 @@
+#Introduction to Python Control Flow and Looping
+num=int(input("Enter a Number: "))
+if (num<100):
+    print("The Number is Less than 100")
+elif (num==100):
+    print("The Number is equal to 100")
+else:
+    print("The Number is more than 100")
+
+if num:
+    pass # pass is used to skip the statements in the script in control structure 
+else:
+    print("It is False")
+
+'''
+Created on 09-May-2018
+
+@author: RAJESH
+'''
