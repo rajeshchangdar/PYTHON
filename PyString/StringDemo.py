df5c1a386ac83e86105650ce2c0ce2c7022d5e8b
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-15 17:11:26 +0530

    modified again

--- PyString/StringDemo.py
+++ PyString/StringDemo.py
@@ -1,8 +1,22 @@
-#Introduction to Python String
+#Introduction to basic Python String operations
+str1="This is Rajesh"
+print(str1)
 
+str2="This is a Good Student\
+who works in TCS Kolkata"
+print(str2)
 
-print("This is the String Printing in Python Language")
+str3="This is an engineer\nThis is an Doctor"
+print(str3)
 
+str4="""I am an Engineer 
+who works as a Software Engineer
+in Price WaterHouse Coopers"""
+print(str4)
+
+str5="Rajesh""Changdar"
+print(str5)
+print(len(str5))
 
 '''
 Created on 13-May-2018
