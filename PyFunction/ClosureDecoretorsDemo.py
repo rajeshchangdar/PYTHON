6297159dca1d259e2c407b9fba971a50b515a779
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-22 11:24:07 +0530

    map() function demo is added

---
+++ PyFunction/ClosureDecoretorsDemo.py
@@ -0,0 +1,22 @@
+#Introduction to Map Function
+"""
+# Map takes one function and one iterator as input and operates on the iterator
+print(":::::::MAP():::::::")
+def findpower(num):
+    return(num**2)
+
+if(__name__=='__main__'):
+    lst=[1,2,3,4]
+    print(list(map(findpower,lst)))
+   
+"""
+#Introduction to Closure
+
+
+
+
+'''
+Created on 21-May-2018
+
+@author: RAJESH
+'''
