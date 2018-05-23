c95171dd695bd463be59b1ad376a87a147815f48
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-23 08:25:20 +0530

    just indroducing python file-

--- PyFileHandling/FileDemo.py
+++ PyFileHandling/FileDemo.py
@@ -1,7 +1,11 @@
 # Introduction to Python File
+fileobj=open("D:\Coding Zone\Python-Home\Python-Introduction\PyFileHandling\mydemo.txt","r")
 
+#print(fileobj)
 
+#fileobj.close()
 
+fileobj.read()
 
 '''
 Created on 22-May-2018
