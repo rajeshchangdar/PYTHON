b275ed5db145e034c9a0ef5a98cd71fdbbc9e469
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-23 11:50:13 +0530

    Object Deleting and Getter Setter examples are added

---
+++ PyClass/ClassFeatures.py
@@ -0,0 +1,39 @@
+#Here are some features of class
+"""
+#Example of deleting an object
+class Calc:
+    def __init__(self):
+        
+        print("One Claculator object is created")
+    
+    def add(self,num1,num2):
+        return(num1+num2)
+
+
+calcobj=Calc()
+
+print(calcobj.add(12, 18))
+
+del(calcobj)
+
+print(calcobj)
+
+"""
+#Getters and Setters in Python
+class Student(object):
+    def __init__(self,name):
+        self.name=name
+
+stdobje=Student("Rajesh Changdar")
+
+print(stdobje.name)
+
+stdobje.name="Tamal Khamrui"
+
+print(stdobje.name)
+
+'''
+Created on 23-May-2018
+
+@author: RAJESH
+'''
