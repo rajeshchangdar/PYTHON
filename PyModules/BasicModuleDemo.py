1858ae5f0fb80cae62952fad95f64fe74e244ed7
RAJESH <RAJESH@DESKTOP-D376QSE>
2018-05-23 21:28:34 +0530

    basic module,submodule and their use demo added

---
+++ PyModules/BasicModuleDemo.py
@@ -0,0 +1,17 @@
+#Introduction to Python Modules
+import calcmodule as cm
+import sys
+print(cm.add(12, 13))
+
+
+print(sys.exit())
+
+print(cm.mult(15, 5))
+
+
+
+'''
+Created on 23-May-2018
+
+@author: RAJESH
+'''
---
+++ PyModules/calcmodule.py
@@ -0,0 +1,20 @@
+#This is my Calculator Module just for demonstration purpose
+
+def add(num1,num2):
+    return(num1+num2)
+
+def sub(num1,num2):
+    return(num1-num2)
+
+def mult(num1,num2):
+    return(num1*num2)
+
+def div(num1,num2):
+    return(num1/num2)
+
+
+'''
+Created on 23-May-2018
+
+@author: RAJESH
+'''
